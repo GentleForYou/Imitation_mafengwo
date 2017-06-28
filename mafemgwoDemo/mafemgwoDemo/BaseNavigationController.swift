11a641dccf00429482e30a153b0e3a87f16404a9
邵文乐 <1035741979@qq.com>
2017-06-28 23:57:19 +0800

    文件结构目录

---
+++ mafemgwoDemo/mafemgwoDemo/BaseNavigationController.swift
@@ -0,0 +1,43 @@
+//
+//  BaseNavigationController.swift
+//  VacationTour
+//
+//  Created by 出神入化 on 2017/6/20.
+//  Copyright © 2017年 出神入化. All rights reserved.
+//
+
+import UIKit
+
+class BaseNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard  let targets = interactivePopGestureRecognizer?.value(forKey: "_targets") as?[AnyObject] else {
+            return
+        }
+        
+        let dict = targets[0]
+        let target = dict.value(forKey: "target") as Any
+        let action = Selector(("handleNavigationTransition:"))
+        let gesture = UIPanGestureRecognizer(target: target, action: action)
+        view.addGestureRecognizer(gesture)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
